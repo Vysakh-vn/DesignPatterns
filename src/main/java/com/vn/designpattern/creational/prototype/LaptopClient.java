4bda2155655e27529bce1e0887775a349802c7ed
Vysakh-vn <vysakhvn28@gmail.com>
2025-01-28 14:13:53 +0530

    Added comments for prototype implementation

--- src/main/java/com/vn/designpattern/creational/prototype/LaptopClient.java
+++ src/main/java/com/vn/designpattern/creational/prototype/LaptopClient.java
@@ -9,12 +9,21 @@ public class LaptopClient {
 
 	public static void main(String args[]) {
 		 
+		/*
+		 * Here we are creating two laptop instance using the regular constructor. Since laptop instance creation is a time consuming process 
+		 * creating two instances will take significant amount of time and affect perfomance.
+		 * 		 */
 		long startTime, endTime;
 		startTime = System.currentTimeMillis();
 		Laptop laptop1 = new Laptop(512, 8, "Intel i3", "DELL", "Black");
 		Laptop laptop2 = new Laptop(256, 4 , "Intel i3", "Dell", "Black");
 		endTime = System.currentTimeMillis();
 		LOGGER.log(Level.INFO, "Two laptop created in : {0} milliseconds", endTime - startTime);
+		
+		/*
+		 * Below we are using clone method to create the clone of the already created laptop instance and then modifying only the required values,
+		 * since we are cloning there will be significant improvement in perfomance.
+		 */
 		
 		startTime = System.currentTimeMillis();
 		Laptop laptop3 = laptop1.clone();
