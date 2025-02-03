77e640dd1158d5ea1a0b2bb65a9cf952fe71f996
Vysakh-vn <vysakhvn28@gmail.com>
2025-02-03 14:07:11 +0530

    Minor updation

--- src/main/java/com/vn/designpattern/creational/abstractfactory/GamingLaptopFactory.java
+++ src/main/java/com/vn/designpattern/creational/abstractfactory/GamingLaptopFactory.java
@@ -2,6 +2,7 @@ package com.vn.designpattern.creational.abstractfactory;
 
 public class GamingLaptopFactory implements LaptopFactory {
 	
+	@Override
 	public Laptop createLaptop() {
 		return new GamingLaptop();
 	}
